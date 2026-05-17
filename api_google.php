57709315f9abfcfc526563efd311c9e86fc7889a
tiagojunior <tiagojr159@hotmail.com>
2026-05-17 13:35:05 -0300

    ajustado

--- api_google.php
+++ api_google.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 /* ======================================================
-   CONEXÃO ROBUSTA (INDEPENDE DO conexao.php)
+   CONEXÃO ROBUSTA 
 ====================================================== */
 $conn = null;
 
